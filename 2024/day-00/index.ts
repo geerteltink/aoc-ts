ff3b3664bff4e34edba9dcff6349818844670ad7
Geert Eltink <geert.eltink@gmail.com>
2024-12-03 07:34:58 +0100

    feat: have 2 test inputs ready

--- 2024/day-00/index.ts
+++ 2024/day-00/index.ts
@@ -1,5 +1,6 @@
 import input from './input.txt';
-import testInput from './input.test.txt';
+import testInput1 from './input1.test.txt';
+import testInput2 from './input2.test.txt';
 import { testSolution, type Solution, solve } from '@lib';
 
 const EXPECTED_PART_ONE = undefined;
@@ -16,13 +17,13 @@ const part2 = async (_data: string): Promise<Solution> => {
 export const main = async (year: string, day: string) => {
     return Promise.resolve()
         .then(function () {
-            return testSolution(year, day, part1, testInput, EXPECTED_PART_ONE);
+            return testSolution(year, day, part1, testInput1, EXPECTED_PART_ONE);
         })
         .then(function () {
             return solve(year, day, part1, input);
         })
         .then(function () {
-            return testSolution(year, day, part2, testInput, EXPECTED_PART_TWO);
+            return testSolution(year, day, part2, testInput2, EXPECTED_PART_TWO);
         })
         .then(function () {
             return solve(year, day, part2, input);
